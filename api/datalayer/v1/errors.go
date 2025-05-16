522046c4c7bcb1d2ebb2d3a1485e116bcb4f3ad8
zhoudongbin <458405826@qq.com>
2025-05-16 10:09:43 +0800

    fea: define errors

---
+++ api/datalayer/v1/errors.go
@@ -0,0 +1,24 @@
+package v1
+
+const (
+	ReasonInvalidArgument     = "INVALID_ARGUMENT"
+	ReasonInvalidJoin         = "INVALID_JOIN"
+	ReasonInvalidAggregation  = "INVALID_AGGREGATION"
+	ReasonInvalidWhereClause  = "INVALID_WHERE_CLAUSE"
+	ReasonInvalidHavingClause = "INVALID_HAVING_CLAUSE"
+
+	ReasonQueryFailed  = "QUERY_FAILED"
+	ReasonNotFound     = "NOT_FOUND"
+	ReasonInsertFailed = "INSERT_FAILED"
+	ReasonDuplicate    = "DUPLICATE"
+	ReasonUpdateFailed = "UPDATE_FAILED"
+	ReasonDeleteFailed = "DELETE_FAILED"
+
+	ReasonTransactionError          = "TRANSACTION_ERROR"
+	ReasonTransactionCommitFailed   = "TRANSACTION_COMMIT_FAILED"
+	ReasonTransactionRollbackFailed = "TRANSACTION_ROLLBACK_FAILED"
+	ReasonInvalidTransactionID      = "INVALID_TRANSACTION_ID"
+
+	ReasonListTablesFailed     = "LIST_TABLES_FAILED"
+	ReasonDescribeTablesFailed = "DESCRIBE_TABLE_FAILED"
+)
