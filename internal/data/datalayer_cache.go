ca512ec1f5e11e163b5894a5c9615b37648a80a2
zhoudongbin <458405826@qq.com>
2025-07-16 09:56:27 +0800

    cache transaction

--- internal/data/datalayer_cache.go
+++ internal/data/datalayer_cache.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	v1 "datahub/api/datalayer/v1"
 	"datahub/internal/biz"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"time"
 
 	"github.com/go-kratos/kratos/v2/log"
@@ -25,6 +26,18 @@ func NewCachingDatalayerRepo(wrapped *DatalayerRepo, cache *RedisClient, logger 
 		cache:   cache,
 		log:     log.NewHelper(logger),
 	}
+}
+
+func (r *CachingDatalayerRepo) BeginTransaction(ctx context.Context, req *v1.BeginTransactionRequest) (*v1.BeginTransactionResponse, error) {
+	return r.wrapped.BeginTransaction(ctx, req)
+}
+
+func (r *CachingDatalayerRepo) CommitTransaction(ctx context.Context, req *v1.TransactionRequest) (*emptypb.Empty, error) {
+	return r.wrapped.CommitTransaction(ctx, req)
+}
+
+func (r *CachingDatalayerRepo) RollbackTransaction(ctx context.Context, req *v1.TransactionRequest) (*emptypb.Empty, error) {
+	return r.wrapped.RollbackTransaction(ctx, req)
 }
 
 func (r *CachingDatalayerRepo) ListTables(ctx context.Context, req *v1.ListTablesRequest) (*v1.ListTablesResponse, error) {
