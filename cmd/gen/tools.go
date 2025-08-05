a2b975a369763b6f1041b7b4d5457122f25e7558
zhoudongbin <458405826@qq.com>
2025-08-05 10:27:26 +0800

    feat:gen-tools

---
+++ cmd/gen/tools.go
@@ -0,0 +1,30 @@
+package gen
+
+import (
+	"strings"
+	"unicode"
+)
+
+func simpleSnakeToCamel(s string) string {
+	if s == "" {
+		return ""
+	}
+	if strings.ToLower(s) == "id" {
+		return "ID"
+	}
+
+	var result strings.Builder
+	capitalizeNext := true
+
+	for _, r := range s {
+		if r == '_' {
+			capitalizeNext = true
+		} else if capitalizeNext {
+			result.WriteRune(unicode.ToUpper(r))
+			capitalizeNext = false
+		} else {
+			result.WriteRune(r)
+		}
+	}
+	return result.String()
+}
